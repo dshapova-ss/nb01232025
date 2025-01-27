49c63cff714b4ded10548830442d6545ace95a1c
dshapova-ss <dshapova@softserveinc.com>
2025-01-27 12:27:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import shutil
 from pathlib import Path
 import logging
+import os
 
+# Set up logging
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(message)s',
@@ -10,24 +12,32 @@ logging.basicConfig(
     ]
 )
 
-# Use user directory as a base to ensure OS compatibility
+# Define paths and vars
 base_dir = Path.home() / "automation_test"
 source_path = base_dir / "source"
 destination_path = base_dir / "destiny"
 
-# Create source directory and some files for demonstration purposes
+# Create source directory
 source_path.mkdir(parents=True, exist_ok=True)
-(destination_path).mkdir(parents=True, exist_ok=True)
 
-shutil.copytree(
-    source_path,
-    destination_path,
-    dirs_exist_ok=True
-)
+# Create a text file with "hello nebo mentor!" content
+(txt_file := source_path / "nebo_text.txt").write_text("hello nebo mentor!")
 
-# Program result without sensitive info 
-home_dir = str(Path.home())
-source_path_str = str(source_path).replace(home_dir, "~")
-destination_path_str = str(destination_path).replace(home_dir, "~")
+# Ensure destination directory exists
+destination_path.mkdir(parents=True, exist_ok=True)
 
-logging.info(f"Files copied successfully from {source_path_str} to {destination_path_str}.")
+logging.info("checking directories before copytree")
+contents = os.listdir(destination_path)
+logging.info(contents)
+logging.info("end of directory")
+
+# Copy source directory contents to destination directory
+shutil.copytree(source_path, destination_path, dirs_exist_ok=True)
+
+#checking directory after
+logging.info("checking directories after copytree")
+contents = os.listdir(destination_path)
+logging.info(contents)
+logging.info("end of directory")
+
+logging.info(f"Files copied successfully from {source_path} to {destination_path}.")
